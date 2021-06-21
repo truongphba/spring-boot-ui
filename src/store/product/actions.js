f64809bc5fdde5bdb652f1d34fab66578e1bd19d
truongphba <truongph.ba@gmail.com>
2021-06-21 20:29:55 +0700

    truongph

--- src/store/product/actions.js
+++ src/store/product/actions.js
@@ -1,19 +1,10 @@
 export async function loadProducts({commit}) {
     commit('fetchProductsBegin')
-    commit('fetchProductsSuccess', {
-        data: [
-            {
-                id: 1,
-                name: 'product1',
-                price: 1000
-            }
-        ]
-    })
-    // fetch('http://127.0.0.1:8081/api/students')
-    //     .then(response => response.json())
-    //     .then((data) => {
-    //         commit('fetchStudentsSuccess', {
-    //             data: data
-    //         })
-    //     })
+    fetch('http://127.0.0.1:8081/api/products')
+        .then(response => response.json())
+        .then((data) => {
+            commit('fetchProductsSuccess', {
+                data: data
+            })
+        })
 }
